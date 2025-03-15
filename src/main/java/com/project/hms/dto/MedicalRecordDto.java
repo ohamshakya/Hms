7763df5698a7fb93249b14383fe9013c87c47f07
Oham Shakya <ohamshakya40@gmail.com>
2025-03-15 21:54:28 +0545

    medical records added

---
+++ src/main/java/com/project/hms/dto/MedicalRecordDto.java
@@ -0,0 +1,22 @@
+package com.project.hms.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class MedicalRecordDto {
+    private Integer id;
+
+    private String diagnosis;
+
+    private String treatment;
+
+    private LocalDate recordDate;
+}
---
+++ src/main/java/com/project/hms/entity/MedicalRecord.java
@@ -0,0 +1,27 @@
+package com.project.hms.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Table(name = "medical_record_details")
+public class MedicalRecord {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String diagnosis;
+
+    private String treatment;
+
+    private LocalDate recordDate;
+}
