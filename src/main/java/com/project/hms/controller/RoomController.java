e9a46496923fb9594e30e8dc363e4b07f13b73fc
Oham Shakya <ohamshakya40@gmail.com>
2025-03-30 23:00:25 +0545

    room message added

--- src/main/java/com/project/hms/controller/RoomController.java
+++ src/main/java/com/project/hms/controller/RoomController.java
@@ -1,17 +1,15 @@
 package com.project.hms.controller;
 
+import com.project.hms.common.utils.Messages;
 import com.project.hms.common.utils.PaginationUtils;
 import com.project.hms.common.utils.ResponseWrapper;
 import com.project.hms.dto.RoomDto;
-import com.project.hms.entity.Room;
 import com.project.hms.service.RoomService;
 import jakarta.validation.Valid;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
-import javax.swing.text.html.Option;
 import java.util.List;
 import java.util.Optional;
 
@@ -33,33 +31,33 @@ public class RoomController {
     @PostMapping
     public ResponseWrapper<RoomDto> addRoom(@Valid @RequestBody RoomDto roomDto) {
         RoomDto response = roomService.add(roomDto);
-        return new ResponseWrapper<>(response,"created successfully", HttpStatus.CREATED.value());
+        return new ResponseWrapper<>(response, Messages.ROOM_ADDED_SUCCESSFULLY, HttpStatus.CREATED.value());
     }
 
     @PutMapping("{id}")
     public ResponseWrapper<RoomDto> updateRoom(@PathVariable int id, @Valid @RequestBody RoomDto roomDto) {
         RoomDto response = roomService.update(roomDto, id);
-        return new ResponseWrapper<>(response,"updated successfully", HttpStatus.OK.value());
+        return new ResponseWrapper<>(response, Messages.ROOM_UPDATED_SUCCESSFULLY, HttpStatus.OK.value());
     }
 
     @GetMapping("{id}")
     public ResponseWrapper<RoomDto> getRoom(@PathVariable int id) {
         RoomDto response = roomService.getById(id);
-        return new ResponseWrapper<>(response,"get successfully", HttpStatus.OK.value());
+        return new ResponseWrapper<>(response, Messages.ROOM_RETRIEVED_SUCCESSFULLY, HttpStatus.OK.value());
     }
 
     @GetMapping
     public ResponseWrapper<List<RoomDto>> getRooms() {
         List<RoomDto> response = roomService.getAll();
-        return new ResponseWrapper<>(response,"get successfully", HttpStatus.OK.value());
+        return new ResponseWrapper<>(response, Messages.ROOM_RETRIEVED_SUCCESSFULLY, HttpStatus.OK.value());
     }
 
     @GetMapping("/search")
-    public ResponseWrapper<Object> search(@RequestParam("query")Optional<String> query,
-                                                 @RequestParam("page")Optional<Integer> page,
-                                                 @RequestParam("size")Optional<Integer> size,
-                                                 @RequestParam("sortBy")Optional<String> sortBy,
-                                                 @RequestParam("sortOrder")Optional<String> sortOrder) {
+    public ResponseWrapper<Object> search(@RequestParam("query") Optional<String> query,
+                                          @RequestParam("page") Optional<Integer> page,
+                                          @RequestParam("size") Optional<Integer> size,
+                                          @RequestParam("sortBy") Optional<String> sortBy,
+                                          @RequestParam("sortOrder") Optional<String> sortOrder) {
         Pageable pageable = PaginationUtils.preparePagination(
                 page,
                 size.orElse(DEFAULT_PAGE),
@@ -69,12 +67,12 @@ public class RoomController {
         String q;
         Object response;
 
-        if(query.isPresent()) {
+        if (query.isPresent()) {
             q = query.get();
             response = roomService.search(q, pageable);
-        }else{
+        } else {
             response = roomService.getAll();
         }
-        return new ResponseWrapper<>(response,"retrieved",HttpStatus.OK.value());
+        return new ResponseWrapper<>(response, Messages.ROOM_RETRIEVED_SUCCESSFULLY, HttpStatus.OK.value());
     }
 }
