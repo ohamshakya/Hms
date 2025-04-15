628a8b501197d80df1543ea065f841c389aae031
Oham Shakya <ohamshakya40@gmail.com>
2025-04-15 23:02:36 +0545

    prescription update and get

--- src/main/java/com/project/hms/controller/PrescriptionController.java
+++ src/main/java/com/project/hms/controller/PrescriptionController.java
@@ -5,8 +5,7 @@ import com.project.hms.dto.PrescriptionDto;
 import com.project.hms.service.PrescriptionService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/prescription")
@@ -18,9 +17,24 @@ public class PrescriptionController {
         this.prescriptionService = prescriptionService;
     }
 
-    public ResponseWrapper<PrescriptionDto> create(PrescriptionDto prescriptionDto) {
+    @PostMapping
+    public ResponseWrapper<PrescriptionDto> create(@RequestBody PrescriptionDto prescriptionDto) {
         log.info("Inside create prescription : controller");
         PrescriptionDto response = prescriptionService.create(prescriptionDto);
         return new ResponseWrapper<>(response,"prescription added", HttpStatus.CREATED.value());
+    }
+
+    @PutMapping("{id}")
+    public ResponseWrapper<PrescriptionDto> update(@RequestBody PrescriptionDto prescriptionDto,@PathVariable Integer id){
+        log.info("Inside update prescription : controller");
+        PrescriptionDto updateResponse = prescriptionService.update(id, prescriptionDto);
+        return new ResponseWrapper<>(updateResponse,"prescription updated", HttpStatus.OK.value());
+    }
+
+    @GetMapping("{id}")
+    public ResponseWrapper<PrescriptionDto> getById(@PathVariable Integer id){
+        log.info("Inside get prescription : controller");
+        PrescriptionDto response = prescriptionService.getById(id);
+        return new ResponseWrapper<>(response,"prescription retrieved", HttpStatus.OK.value());
     }
 }
